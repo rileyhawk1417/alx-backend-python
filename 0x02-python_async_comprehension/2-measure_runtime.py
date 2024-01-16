0f45efe56a73988162e34d0fa202e9960e2dbc39
Sean Riley Hawkins <rileyhawk249@gmail.com>
2024-01-16 12:49:26 +0200

    fix: use asyncio.gather instead of for loop

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import time
+import asyncio
 
 async_comprehension = __import__("1-async_comprehension").async_comprehension
 
@@ -10,8 +11,12 @@ async def measure_runtime():
     then measures the time
     """
     start_time = time.time()
-    for _ in range(4):
-        await async_comprehension()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+    )
     stop_time = time.time()
     total_time = stop_time - start_time
     return total_time
