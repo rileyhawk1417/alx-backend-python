de2006062a816db348a108d47e70dab6fdd19b57
Sean Riley Hawkins <rileyhawk249@gmail.com>
2024-02-07 17:06:24 +0200

    chore: complete memoization task

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -15,6 +15,7 @@ from typing import (
 from utils import (
     access_nested_map,
     get_json,
+    memoize,
 )
 
 
@@ -69,6 +70,31 @@ class TestGetJson(unittest.TestCase):
         Test `get_json` output with mock payload
         """
         attr = {'json.return_value': test_payload}
+        # NOTE: Ignore linter warning still passes
         with patch('requests.get', return_value=Mock(**attr)) as req:
             self.assertEqual(get_json(test_url), test_payload)
             req.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """
+    Test case to `memoize` a method
+    """
+
+    def test_memoize(self) -> None:
+        """Test `memoize` output"""
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(TestClass, "a_method", return_value=lambda: 42,) as memo_func:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
+            # NOTE: Just checks if the function was only called once
+            memo_func.assert_called_once()
