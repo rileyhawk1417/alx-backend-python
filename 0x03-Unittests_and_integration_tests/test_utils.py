6e41f2bbc336b5658e846a781b234575faf63967
Sean Riley Hawkins <rileyhawk249@gmail.com>
2024-02-07 17:25:43 +0200

    fix: remove a comma

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -91,7 +91,7 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 return self.a_method()
         with patch.object(TestClass, "a_method",
-                          return_value=lambda: 42,) as memo_func:
+                          return_value=lambda: 42) as memo_func:
             test_class = TestClass()
             self.assertEqual(test_class.a_property(), 42)
             self.assertEqual(test_class.a_property(), 42)
