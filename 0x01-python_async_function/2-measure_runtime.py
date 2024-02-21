6eaceffaf4001bb9e42983982732d4dabbc4068d
Sean Riley Hawkins <rileyhawk249@gmail.com>
2024-02-21 16:30:45 +0200

    fix: remove async on function

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -7,7 +7,7 @@ import asyncio
 wait_n = __import__("1-concurrent_coroutines").wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """The function measures the runtime
     n: int
     max_delay: int
